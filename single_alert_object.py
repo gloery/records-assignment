a2fb00e8e384b2677bad64bc67ac4e3b4e4a2a83
Gordon Loery <gloery945@gmail.com>
2015-02-16 21:23:54 -0600

    added comments to alert class

--- single_alert_object.py
+++ single_alert_object.py
@@ -1,6 +1,8 @@
 import datetime
 
 class AlertObj:
+    """Object to represent one record (which is one line) in an ASCII file.
+    """
     source_ip = 0
     source_port = 0
     destination_ip = 0
@@ -12,4 +14,5 @@ class AlertObj:
         self.source_port = source_port
         self.destination_ip = destination_ip
         self.destination_port = destination_port
+        # convert a string of the format "year month day hour:minute:second" into a datetime object 
         self.start_time = datetime.datetime.strptime(start_time, '%Y %b %d %H:%M:%S')
